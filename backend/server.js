36a4ec70889dceb4d37587b60bf7a630d6fb9d22
AzmitaSaifani <saifaniazmita@gmail.com>
2025-11-27 17:28:45 +0700

    fasilitator dan penyelenggara

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import authRoutes from "./routes/index.js";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static('uploads'));
-
+app.use("/uploads", express.static("uploads"));
+// === STATIC FILE SERVING UNTUK FOTO ===
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
   res.send("✅ Server API pelatihan jalan!");
